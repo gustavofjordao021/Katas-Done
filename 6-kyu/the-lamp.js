a6b2a67f37ab3e6a09d3c108ae3cc3155d3dd035
Gustavo Jordao <gustavo.jordao021@gmail.com>
2020-01-11 17:02:53 -0500

    Initial work on Lamp kata

---
+++ 6-kyu/the-lamp.js
@@ -0,0 +1,26 @@
+// link: https://www.codewars.com/kata/570e6e32de4dc8a8340016dd/train/javascript
+// Define a class called Lamp. It will have a string attribute for color and boolean attribute, on, that will refer to whether the lamp is on or not. Define your class constructor with a parameter for color and assign on as false on initialize.
+// Give the lamp an instance method called toggleSwitch that will switch the value of the on attribute.
+// Define another instance method called state that will return "The lamp is on." if it's on and "The lamp is off." otherwise.
+
+class Lamp {
+    constructor(color) {
+        this.color = "Blue";
+        this.on = false
+    }
+
+    toggleSwitch = () => {
+        this.on = true;
+    }
+
+    state = () => {
+        switch (this.color) {
+            case true:
+                return "The lamp is on";
+                break;
+            case false:
+                return "The lamp is off";
+                break;
+        }
+    }
+}
