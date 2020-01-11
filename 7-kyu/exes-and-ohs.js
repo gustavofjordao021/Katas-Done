868ce6e778929ffc56a9c5479fb14fcb1cb7bb78
Gustavo Jordao <gustavo.jordao021@gmail.com>
2020-01-11 11:57:03 -0500

    Update link

--- 7-kyu/exes-and-ohs.js
+++ 7-kyu/exes-and-ohs.js
@@ -1,4 +1,4 @@
-//link:
+//link: https://www.codewars.com/kata/55908aad6620c066bc00002a
 /*Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
 
 Examples input/output:
