4c40006dbdf3996a4c01286dd3726c4d8e3580e9
Gustavo Jordao <gustavo.jordao021@gmail.com>
2022-12-02 10:30:59 -0500

    Finish kata

---
+++ 7-kyu/how-many-arguments.js
@@ -0,0 +1,9 @@
+//link: https://www.codewars.com/kata/5c44b0b200ce187106452139/
+/* 
+Targs_count(1, 2, 3) -> 3
+args_count(1, 2, 3, 10) -> 4
+*/
+
+function args_count() {
+	return arguments.length;
+}
