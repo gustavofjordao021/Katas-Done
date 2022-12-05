10db8cd6064f41cda58b61059f965e8cf1a0fc1a
Gustavo Jordao <gustavo.jordao021@gmail.com>
2022-12-05 13:50:02 -0500

    Add kata description

--- 7-kyu/find-the-stray-number.js
+++ 7-kyu/find-the-stray-number.js
@@ -1,7 +1,14 @@
-//link: https://www.codewars.com/kata/5c44b0b200ce187106452139/
+//link: https://www.codewars.com/kata/57f609022f4d534f05000024
 /* 
-Targs_count(1, 2, 3) -> 3
-args_count(1, 2, 3, 10) -> 4
+You are given an odd-length array of integers, in which all of them are the same, except for one single number.
+
+Complete the method which accepts such an array, and returns that single different number.
+
+The input array will always be valid! (odd-length >= 3)
+
+Examples
+[1, 1, 2] ==> 2
+[17, 17, 3, 17, 17, 17, 17] ==> 3
 */
 
 function stray(numbers) {
